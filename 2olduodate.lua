0201e3f870c36145ed4788c9bd638661b3f48877
XspeedHub <eeuuleyenda@gmail.com>
2026-02-01 20:13:18 -0600

    Update 2olduodate.lua

--- 2olduodate.lua
+++ 2olduodate.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/Zlhub/refs/heads/main/2%20oldupdate%20loader"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/Zlhub/refs/heads/main/2oldupdateLoader"))()
