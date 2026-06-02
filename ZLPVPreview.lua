cfb21e2a02f238afc8566a4385483ac5c50d261d
XspeedHub <eeuuleyenda@gmail.com>
2026-06-02 00:21:44 -0600

    Update ZLPVPreview.lua

--- ZLPVPreview.lua
+++ ZLPVPreview.lua
@@ -8,7 +8,7 @@ pcall(function()
 end)
 
 pcall(function()
-        local ZL = "https://raw.githubusercontent.com/xspeedHub0/no/main/marcaagua.lua"
+       -- local ZL = "https://raw.githubusercontent.com/xspeedHub0/no/main/marcaagua.lua"
 loadstring(game:HttpGet(ZL))()
 
 queue_on_teleport(string.format([[
