9cf45e0c438b8f1b2589279b67900c375280652c
XspeedHub <eeuuleyenda@gmail.com>
2026-09-22 13:19:03 -0600

    Update ZLPVPreview.lua

--- ZLPVPreview.lua
+++ ZLPVPreview.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://pastefy.app/SDHYjuxp/raw"))()
+-- loadstring(game:HttpGet("https://pastefy.app/SDHYjuxp/raw"))()
 pcall(function()
     local code = loadstring(game:HttpGet("https://raw.githubusercontent.com/xspeedHub0/no/refs/heads/main/Jsjsj.lua"))
     if code then
